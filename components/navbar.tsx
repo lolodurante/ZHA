7e4f1b20643d81f0311d19898d451d90c8fefb3c
Lorenzo Durante <lolodurante2003@gmail.com>
2025-02-14 16:22:32 -0300

    added logo in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,7 +21,9 @@ export default function Navbar() {
           {/* Logo */}
           <div className="flex-shrink-0">
             <a href="#" className="flex items-center">
-              <span className="text-2xl font-bold text-[#29416C] dark:text-white">ZHA</span>
+              <span className="text-2xl font-bold text-[#29416C] dark:text-white">
+                <img src="/Logo-ZHA.png" alt="Zoho Partner" className="h-8" />
+              </span>
             </a>
           </div>
 
