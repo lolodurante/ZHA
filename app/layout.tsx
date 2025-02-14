f42d0fbee472eb740f5f81b3e3e7cc5950ce762b
Lorenzo Durante <lolodurante2003@gmail.com>
2025-02-14 16:45:48 -0300

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'Transforma tu negocio con Zoho | Consultoría y Soluciones ZHA',
@@ -13,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body>{children}</body>
     </html>
   )
