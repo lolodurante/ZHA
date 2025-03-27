aa11a6be6bda9e84dda45976e70c4fa0fd703f1f
Lorenzo Durante <lolodurante2003@gmail.com>
2025-03-27 15:12:52 -0300

    added pagesense

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Analytics } from "@vercel/analytics/react"
 import Script from 'next/script'
 
 export const metadata: Metadata = {
-  title: 'ZHA | Zoho Affiliate',
+  title: 'ZHA | Zoho Affiliate',
   description: 'Optimiza tu empresa con las soluciones de Zoho. Consultoría, CRM, automatización y más. ¡Obtén $100 USD de regalo al iniciar!',
   icons: {
     icon: '/favicon.ico',
@@ -26,6 +26,11 @@ export default function RootLayout({
           id="zoho-salesiq"
           src="https://salesiq.zohopublic.com/widget?wc=siq249ba8539d877c3af0782842d4e76581"
           strategy="lazyOnload"
+        />
+        <Script
+          id="pagesense-script"
+          src="https://cdn.pagesense.io/js/zhaportal/6fadfdea18aa44a5a6312607ed85bd8e.js"
+          strategy="afterInteractive"
         />
       </head>
       <Analytics />
